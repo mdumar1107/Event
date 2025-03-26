00c4bb2b8311c57c0741107dfe36621e3a2f9df6
mdumar1107 <mdumar1106@gmail.com>
2025-03-26 11:29:22 +0530

    no change

--- react_js_frontend/tailwind.config.js
+++ react_js_frontend/tailwind.config.js
@@ -1,19 +1,17 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        primary: '#7848F4',    // Purple
-        black: '#131315',      // Black
-        white: '#FFFFFF',      // White
-        navy: '#10107B',       // Navy Blue
-        background: '#F8F8FA', // Background Grey
-      }
-    },
+export const content = [
+  "./src/**/*.{js,jsx,ts,tsx}",
+];
+export const theme = {
+  extend: {
+    colors: {
+      primary: '#7848F4', // Purple
+      black: '#131315', // Black
+      white: '#FFFFFF', // White
+      navy: '#10107B', // Navy Blue
+      background: '#F8F8FA', // Background Grey
+    }
   },
-  plugins: [],
-}
+};
+export const plugins = [];
 
